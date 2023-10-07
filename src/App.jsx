bca44e9e14b851a85b962b721a66d5191c2c308a
SiLLeCToff <sillect1337@gmail.com>
2023-10-07 13:46:44 +0300

    update rename MainPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import MainPage from "./components/mainPage/mainPage";
+import MainPage from "./components/MainPage/MainPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
